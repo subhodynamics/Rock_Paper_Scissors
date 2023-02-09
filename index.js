aeb86e43878425e140dcde479fb13ec33c898d94
subhodynamics <subhadeeppathak2013@gmail.com>
2023-02-09 17:27:52 +0530

    dynamic UI

--- index.js
+++ index.js
@@ -1,59 +1,15 @@
-let playerScore = 0;
-let compScore = 0;
-
-function getComputerChoice() {
-    let arr = ["rock", "paper", "scissors"];
-    let choice = Math.floor(Math.random()*3);
-    return arr[choice];
-}
-
-function winnerCC(cc, pc) {
-    compScore++;
-    console.log(`You Lose! ${cc} beats ${pc}`);
-}
-
-function winnerPC(cc, pc) {
-    playerScore++;
-    console.log(`You Won! ${pc} beats ${cc}`);
-}
-// pc = player's choice
-
-function playRound(pc) {
-
-    let cc = getComputerChoice(); //computer choice
-    
-    let ans = true;
-    switch (ans) {
-        case (pc === cc):
-            console.log("Its a tie !");
-            break;
-        case (  (cc === "rock" && pc === "scissors") ||
-                (cc === "scissors" && pc === "paper") ||
-                (cc === "paper" && pc === "rock")   ):
-            
-            winnerCC(cc,pc);
-            // checkScore(playerScore, compScore);
-            break;
-    
-        case ( (cc != "rock") && (cc != "paper") && (cc != "scissors") 
-            || (pc != "rock") && (pc != "paper") && (pc != "scissors") ):
-            
-            console.log("please enter correct choice! (rock / paper/ scissors)");
-            break;
-            
-        default:
-            winnerPC(cc,pc);
-    }
-}
-
-// for (let i = 0; i < 5; i++) playRound();
-
+var playerScore = 0;
+var compScore = 0;
+let winner ='';
+let finalWinner = '';
 
 let rock_btn = document.getElementById('rock');
 let paper_btn = document.getElementById('paper');
 let scissors_btn = document.getElementById('scissors');
 
-console.log(rock_btn);
+const scoreContainer = document.getElementById('score');
+const finalWinContainer = document.getElementById('finalWin');
+
 
 rock_btn.addEventListener('click', function(){
     playRound('rock');
@@ -66,3 +22,68 @@ paper_btn.addEventListener('click', function(){
 scissors_btn.addEventListener('click', function(){
     playRound('scissors');
 });
+
+function getComputerChoice() {
+    let arr = ["rock", "paper", "scissors"];
+    let choice = Math.floor(Math.random()*3);
+    return arr[choice];
+}
+
+// pc = player's choice
+function playRound(pc) {
+    let cc = getComputerChoice(); //computer choice
+    
+    switch (true) {
+
+        //? case for tie
+        case (pc === cc):
+            winner = 'tie';
+            break;
+
+        //? case for computer win
+        case (  (cc === "rock" && pc === "scissors") ||
+                (cc === "scissors" && pc === "paper") ||
+                (cc === "paper" && pc === "rock")   ):
+            winner = 'comp';
+            break;
+
+        //? case for player win
+        default:
+            winner = 'player';
+    }
+    scoreWinner();
+    console.log(winner);
+}
+
+
+function scoreWinner() {
+    if (winner === 'comp') {
+        compScore++;
+        scoreContainer.textContent = `You = ${playerScore}\nComputer = ${compScore}`;
+    }
+    else if (winner === 'player') {
+        playerScore++;
+        scoreContainer.textContent = `You = ${playerScore}\nComputer = ${compScore}`;
+    }
+
+    if (compScore === 5 || playerScore === 5)
+    getWinner();
+}
+
+function getWinner() {
+    switch(true) {
+        case (compScore === 5):
+            console.log('computer wins');
+            break;
+        case (playerScore === 5):
+            console.log('player wins');
+            break;
+        default:
+            console.log('nice');
+    }
+    let final = compScore == 5 ?"computer wins":"player wins";
+    finalWinContainer.textContent += '\n' + final;
+    compScore = 0;
+    playerScore = 0;
+}
+
